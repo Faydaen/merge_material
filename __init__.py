6ed482f19c6bc0143a4689aabf573a98fae4c341
Faydaen <panov.eo@gamail.com>
2025-11-29 20:17:31 +0200

    files

--- __init__.py
+++ __init__.py
@@ -9,17 +9,22 @@ import bpy
 class OT_Test(bpy.types.Operator):
     bl_idname = "object.test_operator"
     bl_label = "Test Operator"
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
         self.report({'INFO'}, "Работает!")
+        print("Работает")
+
         return {'FINISHED'}
 
 def register():
-    print("Addon registered 23")
+    print("Addon registered 1")
     bpy.utils.register_class(OT_Test)
+
 
 def unregister():
     bpy.utils.unregister_class(OT_Test)
 
 if __name__ == "__main__":
     register()
+
